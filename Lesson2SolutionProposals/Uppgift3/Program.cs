66cfa76c51dd5794baf4f39857fcbefb0875b882
Aynamik4 <violintailgut@gmail.com>
2021-01-31 17:55:25 +0100

    Settings changed

---
+++ Lesson2SolutionProposals/Uppgift3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Uppgift3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mata in tal som ska omvandlas till olika enheter: ");
+            string input = Console.ReadLine();
+            double value = double.Parse(input);
+
+            Console.WriteLine($"{value} meter = {Math.Round(value * 1.0936133, 2)} yards");
+        }
+    }
+}
